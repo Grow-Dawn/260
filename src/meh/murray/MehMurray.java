911227fc6348d30926e79e30620e4d84ff11bffd
Grow-Dawn <auroradaun@yahoo.com>
2014-01-23 21:18:11 -0800

    meh murray

--- src/meh/murray/MehMurray.java
+++ src/meh/murray/MehMurray.java
@@ -4,19 +4,80 @@
  * and open the template in the editor.
  */
 
-package meh.murray;
+package memory1;
+
+import java.io.Serializable;
 
 /**
  *
- * @author photos plus
- */
-public class MehMurray {
+ * @author Dawn Grow*/
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+ public static void main(String[] args) {
+ 
+public class Memory1 implements Serializable {
+    private String name;
+    private String instructions = "A player tries to match tiles.   If the tiles chosen match, they will disappear.  Find all 8 matches before time runs out.  Need help?  Click on one tile and click on “hint”.  “OK”.";
+    private long wins = 0;
+    private long losses = 0;
+    private long ties = 0;
+    private String letter = "";
+
+    public Memory1() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public long getWins() {
+        return wins;
+    }
+
+    public void setWins(long wins) {
+        this.wins = wins;
+    }
+
+    public long getLosses() {
+        return losses;
+    }
+
+    public void setLosses(long losses) {
+        this.losses = losses;
+    }
+
+    public long getTies() {
+        return ties;
+    }
+
+    public void setTies(long ties) {
+        this.ties = ties;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+    
+    
+    
+    
+    
+        
     }
     
 }
