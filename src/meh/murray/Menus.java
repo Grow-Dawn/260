8de8cc5c8f897336176724479badc37a694c272c
blainepatrick <blainepatrick@netscape.net>
2014-01-25 17:52:46 -0600

    Added Menus class.

---
+++ src/meh/murray/Menus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meh.murray;
+
+/**
+ *
+ * @author patricks
+ */
+public class Menus
+{
+
+    MainMenu Help = new MainMenu();
+    MainMenu Options = new MainMenu();
+    MainMenu Play = new MainMenu();
+    
+}
