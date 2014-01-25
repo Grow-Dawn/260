61fbad517795ef0123d9d97397a049f745b5fee1
blainepatrick <blainepatrick@netscape.net>
2014-01-25 16:38:26 -0600

    Added option variable declarations.

---
+++ src/meh/murray/Options.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meh.murray;
+
+/**
+ *
+ * @author patricks
+ */
+public class Options
+{
+    boolean sound; //Sound is on (true) or off (false)
+    boolean score; //Score is tracked; on (true) or off (false)
+    int matchSeconds = 4; //n seconds allowed to make a match (configurable)   
+}
