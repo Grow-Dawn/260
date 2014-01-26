60bc4785405e78415db46b4169ebf46f288516d9
blainepatrick <blainepatrick@netscape.net>
2014-01-25 23:56:41 -0600

    Added name variable declaration, and updated reference to answer.next.

--- src/meh/murray/Exit.java
+++ src/meh/murray/Exit.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * @author Dawn Grow
  */
 public class Exit {
-    private String name;
+    public String name;
     private String getExit;
     public void getExit(){
         
