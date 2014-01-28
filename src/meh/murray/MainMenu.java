fe35580c2b8795eaff86055219809190a9102588
blainepatrick <blainepatrick@netscape.net>
2014-01-28 15:39:57 -0600

    Added comments to outline reuqirements of paired assignment for lesson 3

--- src/meh/murray/MainMenu.java
+++ src/meh/murray/MainMenu.java
@@ -7,7 +7,15 @@ package meh.murray;
 /**
  *
  * @author patricks
+ * Step 2: Brainstorm and come up with an idea for type of function to be developed. This function must perform some type of calculation and incorporate the following concepts:
+ * 
+ * Two or more primitive variable types.
+ * Two or more mathematical operators.
+ * At least one relational or logical operator.
+ * A type conversion using type casting.
+ * At least two character escape sequences.
  */
+
 public class MainMenu
 {
     //just an exercise... the menus will be created using Swing later in the course...
