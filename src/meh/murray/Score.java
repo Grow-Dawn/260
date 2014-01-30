2adeb626d7bf79f8b6e53c65a86f4bdda1f50e50
blainepatrick <blainepatrick@netscape.net>
2014-01-30 01:43:06 -0600

    Update Score.java
    Meh

--- src/meh/murray/Score.java
+++ src/meh/murray/Score.java
@@ -8,31 +8,66 @@ package meh.murray;
 
 /**
  *
- * @author Dawn Grow
+ * @author Dawn Grow, PBS
+ * 
  */
 public class Score {
  //private String getScore;
-    public void getScore(){
-        int tries = 0, timer = 0, finalScore, underScore, topScore;
+    public String getScore(){
+        int tries = (int)(10 * Math.random()), timer = (int)(10 * Math.random()), finalScore, underScore, topScore;
 		finalScore = tries * timer;
-        System.out.println("Your score is " + finalScore);
+                //System.out.println(tries.ToString() + "\n" + timer.ToString());
+                String scoreMsg = "\"Great Score!!!\"";
+        System.out.println("\"Your score is " + finalScore + "\"");
 		if(finalScore  == 0 || finalScore < 30){
-            System.out.println("You are awesome!");
+            //System.out.println("\"You are awesome!\"");
+             scoreMsg = "\n" + "\"You are awesome!\"";
             }
-        else if (finalScore == 30|| finalScore <60) {
-            System.out.println("That was great!  Good job!");
+        else if (finalScore == 30 || finalScore <60) {
+            //System.out.println("\"That was great!  Good job!\"");
+            scoreMsg = "\n"+ "\"That was great!  Good job!\"";
             }
         else if (finalScore == 60 || finalScore <90){
-            System.out.println("You did well!");
+            //System.out.println("\"You did well!\"");
+            scoreMsg = "\"You did well!\"";
            }
         else {
-            System.out.println("That was good but I know you can do better!");
-        }
-        topScore =90;
+            //System.out.println("\"That was good but I know you can do better!\"");
+            scoreMsg = "\"That was good but I know you can do better!\"";
+        
+        topScore = 200;
 	underScore = topScore - finalScore;
-	System.out.println("Your score is " + underScore + " points under the best score!");
+        scoreMsg = "\"Your score is " + underScore + " points under the best score!\"";
+	//System.out.println("\"Your score is " + underScore + " points under the best score!\"");
+        return scoreMsg;
 	}
+                
+        return scoreMsg;
     }
-		
+    
+    
+    /**
+    *
+    * @author Patrick Stamps
+    * 
+    */
+    
+//    public int attempts;
+//    public int attemptTime;
+//    public int playerScore;
+//    public String playerMsg = "Your score is ";
+//    public String finalScore;
+//    public String underScore;
+//    public int highScore;
+//        
+//    public String CalcScore(int attemptTime)
+//    {
+//        finalScore = attempts * attemptTime;
+//        playerScore = 0;
+//        return playerMsg;
+//        
+//    }
+    
+}		
 		/*classname.membervariable 
 		try appending .ToString() at the end*/
