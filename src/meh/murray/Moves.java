1e56dae13c756948a6bc19568823a6b33555c44a
Grow-Dawn <auroradaun@yahoo.com>
2014-01-29 18:44:40 -0800

    Create Moves.java

---
+++ src/meh/murray/Moves.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package meh.murray;
+
+/**
+ *
+ * @author Dawn Grow
+ */
+public class Moves {
+ //private String getScore;
+    public void getMoves(){
+        int guess, x, y;
+		// guess will be the number of clicks the player makes
+		guess = 0;
+		//tries = x;
+		//finalScore = tries * timer;
+        System.out.println("What is your first guess?");
+		
+		for(x = 0; x < 100; x++){
+			guess = x/2;
+			return;
+		}
+		System.out.println("Moves:" + guess);
+		
+		
+		}
+    }
+		
+		
