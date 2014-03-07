24f47302e4e9093b5153eb1f76370ac7ddf90b16
Grow-Dawn <auroradaun@yahoo.com>
2014-03-06 22:15:16 -0800

    serialize helpMenu

--- src/meh/murray/HelpMenu.java
+++ src/meh/murray/HelpMenu.java
@@ -4,12 +4,13 @@
  */
 package meh.murray;
 
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
- * @author patricks
+ * @author patricks (and now Dawn)
  */
-public class HelpMenu
-{
+public class HelpMenu implements Serializable {
     boolean enabled;
     boolean main;
     boolean Sub;
@@ -23,7 +24,76 @@ public class HelpMenu
         if(enabled)
         {
             System.out.println(instructions);
+        
         }
     }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isMain() {
+        return main;
+    }
+
+    public void setMain(boolean main) {
+        this.main = main;
+    }
+
+    public boolean isSub() {
+        return Sub;
+    }
+
+    public void setSub(boolean Sub) {
+        this.Sub = Sub;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "HelpMenu{" + "enabled=" + enabled + ", main=" + main + ", Sub=" + Sub + ", instructions=" + instructions + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (this.enabled ? 1 : 0);
+        hash = 71 * hash + (this.main ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final HelpMenu other = (HelpMenu) obj;
+        if (this.enabled != other.enabled) {
+            return false;
+        }
+        if (this.main != other.main) {
+            return false;
+        }
+        if (!Objects.equals(this.instructions, other.instructions)) {
+            return false;
+        }
+        return true;
+    }
+
+    
     
 }
