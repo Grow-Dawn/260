a3967f14b6cdea04ca6d6c38f499cb3ebf01763d
Grow-Dawn <auroradaun@yahoo.com>
2014-03-21 15:56:15 -0700

    indiv packages

---
+++ src/meh/murray/OptionsEnum.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package meh.murray;
+
+/**
+ *
+ * @author photos plus
+ */
+public enum OptionsEnum {
+    setScoreOptions("Keep Score: " + score),
+    setScoreOptions("Keep Score: " + score),
+    setSoundOptions("Soundtrack: " + sound),
+    setSoundOptions("Soundtrack: " + sound),
+    matchSeconds("How many match pairs?\n [4]");
+    String message;
+    
+    private OptionsEnum(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void display() {
+        System.out.println(this.message);
+    }
+}
